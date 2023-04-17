2717f97034f01ad0c19e86ab80af36395daeee24
bondyfriz <anatolybondarev@vk.com>
2023-04-17 11:40:21 +0400

    redact

--- src/lesson_20/lesson_21.java
+++ src/lesson_20/lesson_21.java
@@ -1,8 +1,6 @@
 package lesson_20;
 import java.util.*;
 
-
-
 public class lesson_21 {
     public static void main(String[] args) {
         ArrayList list1 = new ArrayList(); //  создание объекта ArrayList
